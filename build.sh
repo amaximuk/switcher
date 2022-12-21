e1278891c19f9b0e3b2edc5d9958ebea8ddf03a1
amax\alexander <a@b.c>
2022-12-21 09:47:47 +0300

    Рефакторинг

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd build
-cmake -DQt5_DIR=/mnt/data/3rdparty/qt/5.15.2/gcc_64/lib/cmake/Qt5 -G "Unix Makefiles" ..
+cmake -DQt5_DIR=/mnt/data/3rdparty/qt/5.15.2/gcc_64/lib/cmake/Qt5 -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ..
 cd ..
